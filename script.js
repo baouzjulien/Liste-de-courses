08b12ed605f3c507ef2d88beee2302f191a52f5b
Julien Baouz <baouzjulien@gmail.com>
2025-12-27 00:15:16 +0100

    maj drag tactile fin partie appli js vanilla

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function createRayon(nomRayon) {
         <div class="rayon-footer">
             <input type="text" class="nouveau-produit" placeholder="Ajout produit">
             <button class="btn-ajouter-produit">➕</button>
-            <button class="btn-deplacer-produit">↕️</button>
+            <button class="btn-deplacer-produit">déplacer</button>
         </div>
     `;
 
